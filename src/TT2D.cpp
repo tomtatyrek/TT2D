6c57462885e056c78b6eed28e1589c96be75c855
BeeTrout <tomik9737@gmail.com>
2025-03-08 19:25:35 +0100

    Initial commit

---
+++ src/TT2D.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Tomáš Tatyrek on 08/03/2025.
+//
+
+#include "TT2D.h"
+#include <iostream>
+
+void TT2D::hello() {
+	std::cout << "Hello World!" << std::endl;
+}
---
+++ src/TT2D.h
@@ -0,0 +1,12 @@
+//
+// Created by Tomáš Tatyrek on 08/03/2025.
+//
+
+#ifndef TT2D_TT2D_H
+#define TT2D_TT2D_H
+
+namespace TT2D {
+	void hello();
+}
+
+#endif // TT2D_TT2D_H
